455975e48287204ec391ff6badbd663688f8eaff
FaithKwamboka <okongofaith3@gmail.com>
2022-06-22 18:15:06 +0300

    Update files

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,7 @@
-function getname() {
-    const name = document.querySelector('input').value;
-
+document.querySelector("#btn").addEventListener("click", message);
+function message() {
+    let input = document.querySelector("#input").value
+    let generatedElement = document.createElement("p");
+    generatedElement.innerText = `hello ${input} welcome to the bootcamp`
+    document.querySelector('#demo').appendChild(generatedElement);
 }
